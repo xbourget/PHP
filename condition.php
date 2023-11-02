5bc5f317939231c953ec0aa596ece1bef26c9293
lynda <lyndasahli1@hotmail.fr>
2023-11-02 15:58:29 +0100

    Merge xavier

---
+++ condition.php
@@ -0,0 +1,23 @@
+<?php
+
+    $temperature = 15;
+    
+
+    if ( $temperature < -10  )
+    
+        print ("tres tres foid");
+        
+        elseif ( $temperature < 15 )
+        print (" il fait bon" );
+    
+
+        elseif ( $temperature < 30)
+        print ("bon");
+    
+        else
+        print ( "trop chaud" );
+
+
+
+
+?>
---
+++ premier.php
@@ -0,0 +1,14 @@
+<?php
+
+$monNom = 'lynda';
+$age = 43;
+
+print ( 'nom: $monNom<br> age:$age' );
+print ('<br>');
+
+$age = $age + 2;
+print ( "nom: $monNom<br> age:$age" );
+
+
+
+?>
