3e8694ec90cdf86d2a7caa16486b77715488c804
Maria De Carvalho <suzethbarreto@hotmail.com>
2023-11-02 16:19:27 +0100

    totophp

---
+++ condition.php
@@ -0,0 +1,12 @@
+<?php
+$age = 25;
+$nom = 'toto';
+if ( $age >= 18 || $nom == 'toto' )
+{
+    print( "tu peux te sauver en voiture<br>" );
+    print( "bye bye $nom");
+    }
+    elseif ( $age < 5 );
+    print ( "tu peux alle a martenelle");
+   // else print( "tu prend le bus" );
+    ?>
