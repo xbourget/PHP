18676b3ddf59ab9da4397a94ff6e0dbd55d62171
Matman777 <mathias.mauer777@gmail.com>
2023-11-06 08:12:27 +0100

    journéeVendredi0310

--- myLib01M.php
+++ myLib01M.php
@@ -8,6 +8,9 @@
             <meta charset=\"UTF-8\">
             <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
             <title>$titre</title>
+            <style>
+            td: border: 1px solid black;
+            </style>
         </head>
         <body>
         ";
@@ -21,6 +24,38 @@
         </html>
         ";
         print( $var );
-
     }
+
+    function myTable( $width, $height, $smiley=' () ' )
+    {   
+        
+        print( "<table>\n");
+        for( $i=0 ; $i<$height ; $i++)
+        {
+            print( "<tr>\n");
+            for( $j=0 ; $j<$width ; $j++ )
+            {
+                print( "<td>\n");
+                print( $smiley );
+                print( "</td>\n");
+            }
+            print( "</tr>\n");
+        }
+        print( "</table>\n");
+    }
+
+    function myNewTable($persons)
+    {
+        print( "<table>\n");
+        
+        foreach($persons as $person)
+        {   
+            foreach($person as $cellule)
+            print($cellule . ' ');
+            print('<br>');
+        }
+        print( "</table>\n");
+    }
+
+
 ?>
