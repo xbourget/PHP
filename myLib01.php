a92e88456319971ecb51cc65323a4126b936e4e4
“xbourget” <xavier.bourget@gmail.com>
2023-11-07 12:57:45 +0100

    pagePrenom 1

--- myLib01.php
+++ myLib01.php
@@ -44,14 +44,7 @@
     }
 
     function myTableList( $data )
-    {function myFooter()
-        {
-            $var = "
-            </body>
-            </html>
-            ";
-            print( $var );
-        }
+    {
         print( '<table>' );
         for( $i = 0 ; $i < count( $data ) ; $i++ )
         {
