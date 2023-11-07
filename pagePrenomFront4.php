847888d1bd8fac1d4c58f7c47ca1960000a8410d
gabrielhoux <gabriel.houx@gmail.com>
2023-11-07 12:02:18 +0100

    +formulaire age +banana dance

--- pagePrenomFront4.php
+++ pagePrenomFront4.php
@@ -5,12 +5,18 @@
     include "myLib01.php";
     require_once 'ipconfig.php';
 
-    $nom        = $_SESSION[ 'nom' ];
-    $prenom     = $_SESSION[ 'prenom' ];
-    $age        = $_SESSION[ 'age' ];
+    $nom = $_SESSION[ 'nom' ];
+    $prenom = $_SESSION[ 'prenom' ];
+    $age = $_SESSION[ 'age' ];
 
     myHeader("THE END");
 
     print( "<h1>hello $prenom $nom, tu as $age ans, c'est la cour des grands chef</h1>");
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 4; $j++) {
+            print("<img src='".$myAddress."/PHP/banana-dance.gif' alt='banana dance' />");
+        }
+        print("<br>");
+    }
     
 ?>
