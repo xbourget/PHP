7010d2b3e8127dae70e52f4f4dfb76b26d4ad1ff
“xbourget” <xavier.bourget@gmail.com>
2023-11-07 17:09:20 +0100

    formInfo

--- infoBack1.php
+++ infoBack1.php
@@ -2,21 +2,32 @@
     include "parametre.php";
 
     session_start();
+    $_SESSION[ 'message'] = '';
 
     if( !empty($_POST))
     {
         $prenom = $_POST[ 'prenom' ];
         $nom    = $_POST[ 'nom' ];
         $age    = $_POST[ 'age' ];
-        
-        if ( $prenom != "" && $nom != "" && $age != "")
-        {
-            $_SESSION[ 'prenom'  ]  = $prenom;
-            $_SESSION[ 'nom'  ]     = $nom;
-            $_SESSION[ 'age'  ]     = $age;
-            header( 'location: '.$__url.'/PHP/infoFrontFinal.php'   );
-        }
+
+        if ( $prenom == '' )
+            $_SESSION[ 'message' ] = 'tu as oublié ton prenom<br>';
         else
+            $_SESSION[ 'prenom'  ]  = $prenom;
+        
+        if ( $nom == '' )
+            $_SESSION[ 'message' ] .= 'tu as oublié ton nom<br>';
+        else
+            $_SESSION[ 'nom'  ]  = $nom;
+        
+        if ( $age == '' ||  !is_numeric( $age ) )
+            $_SESSION[ 'message' ] .= 'tu as oublié ton age<br>';
+        else
+            $_SESSION[ 'age'  ]  = $age;
+        
+        if ( $_SESSION[ 'message'] != '' )
             header( 'location: '.$__url.'/PHP/infoFront1.php'   );
+        else
+            header( 'location: '.$__url.'/PHP/infoFrontFinal.php'   );
     }
 ?>
