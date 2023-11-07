c5b0df47dc25748e07dfb3920a384d80f3601ca1
Linda kh <lindakhe53@gmail.com>
2023-11-07 11:20:37 +0100

    new merge

--- pagePrenomBack2.php
+++ pagePrenomBack2.php
@@ -7,9 +7,9 @@
         if ( $nom != "" )
         {
             $_SESSION[ 'nom'  ] = $nom;
-            header( 'location: http://192.168.1.152/PHP/pagePrenomFront3.php'   );
+            header( 'location: http://192.168.1.17/PHP/pagePrenomFront3.php'   );
         }
         else
-            header( 'location: http://192.168.1.152/PHP/pagePrenomFront2.html'   );
+            header( 'location: http://192.168.1.17/PHP/pagePrenomFront2.html'   );
     }
 ?>
