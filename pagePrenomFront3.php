847dcc95fe2c9b255b249ffce4af8fb502b9d57d
Maria De Carvalho <suzethbarreto@hotmail.com>
2023-11-08 10:24:34 +0100

    cpoo

--- pagePrenomFront3.php
+++ pagePrenomFront3.php
@@ -3,12 +3,6 @@
     include "parametre.php";
 
     myHeader( 'info nom' );
-<<<<<<< HEAD
-    createForm( $__url."/PHP/pagePrenomBack3.php",  "age", "ton age" );
-    myFooter();
-?>
-=======
     createForm( $__url."/PHP/pagePrenomBack3.php", "age",  "ton age" );
     myFooter();
 ?>
->>>>>>> origin/xavier
