887b84a45ff532134117da25a13240164325a5e8
“xbourget” <xavier.bourget@gmail.com>
2023-11-08 12:27:34 +0100

    POO2

--- POO/forme01.php
+++ POO/forme01.php
@@ -2,11 +2,14 @@
 
 include "triangleLib01.php";
 include "carreLib01.php";
+include "rectangleLib01.php";
 
     $t1 = new Triangle( 5, 9, 12, 6 );
     $t2 = new Triangle( -5, 19, 1, 7 );
 
     $k1 = new Carre( 15, -1, 17 );
+
+    $r1 = new Rectangle( 105, 106, 17, 33 );
 
     $p1 = new Point( 2, 90 );
 
@@ -18,9 +21,32 @@ include "carreLib01.php";
     print( "<br>" );
     print( $k1);
     print( "<br>" );
+    print( $r1);
+    print( "<br>" );
 
     print( $t1->surface() );
     print( "<br>" );
     print( $k1->surface() );
     print( "<br>" );
+
+    print( $r1->surface() );
+    print( "<br>" );
+
+
+
+    $formes = [ $t1, $t2, $k1, $r1 ];
+
+
+    $surfaceTotale = 0;
+    foreach ($formes as $forme ) 
+    {
+        $surfaceTotale += $forme->surface();
+        print( $forme.'<br>' );
+    }
+
+    print( "surface totale : ".$surfaceTotale.'<br>' );
+
+
+
+
 ?>
