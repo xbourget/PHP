f799744f6b17a4e68afbf242dbada7f26aca79a7
Matman777 <mathias.mauer777@gmail.com>
2023-11-08 11:39:50 +0100

    rectangle

--- POO/forme01.php
+++ POO/forme01.php
@@ -2,6 +2,7 @@
 
 include "triangleLib01.php";
 include "carreLib01.php";
+include "rectangleLibM01.php";
 
     $t1 = new Triangle( 5, 9, 12, 6 );
     $t2 = new Triangle( -5, 19, 1, 7 );
@@ -9,6 +10,8 @@ include "carreLib01.php";
     $k1 = new Carre( 15, -1, 17 );
 
     $p1 = new Point( 2, 90 );
+
+    $r1M = new Rectangle(5, 10, 15, 9, "noir");
 
     print( $t1 );
     print( "<br>" );
@@ -23,4 +26,9 @@ include "carreLib01.php";
     print( "<br>" );
     print( $k1->surface() );
     print( "<br>" );
+    print($r1M);
+    print( "<br>" );
+    print($r1M->aire());
+    print( "<br>" );
+    print($r1M->perimetre());
 ?>
