af4c9a86564462d24fb2f3e66ba14487d14161cf
Broustail Robin <grob1012@gmail.com>
2023-11-09 09:01:11 +0100

    popo

---
+++ POO/caisse/article01RB.php
@@ -0,0 +1,24 @@
+<?php
+    class Article
+    {
+        private string $nom;
+        private float $prix;
+        public function __construct( $n, $p ) 
+        {
+            $this->nom  = $n;  // copie des variables 
+            $this->prix = $p;  // dans les variables
+        }      
+        
+        public function __toString() 
+        {
+            return $this->nom.'   '.$this->prix; 
+        }      
+
+        public function getPrix()
+        {
+            return $this->prix;
+        }
+        
+        
+    }
+?>
---
+++ POO/caisse/facture01RB.php
@@ -0,0 +1,40 @@
+<?php
+
+    include_once "article01RB.php";
+
+    class Facture
+    {
+        protected string $client;       // variable d'instance
+        protected $articles = [];       // variable d'instance
+            
+
+        public function numeroFacture() 
+        {
+            print( 'numero de la facture : '.Facture::$numeroFacture );
+        }
+        public function __construct( $c ) 
+        {
+            
+            $this->client  = $c; 
+        }                   
+
+        public function addArticle( Article $art, int $qte ) 
+        {
+            array_push( $this->articles, [ 'art' => $art, 'qte' => $qte ] );
+        }                   
+
+        public function edit( ) 
+        {
+            print(  "client : ".$this->client.'<br>' );
+            print(  '============================<br>' );
+            
+            $cpt = 0;
+            foreach ( $this->articles as $line) 
+            {
+                print(  ++$cpt."  ".$line['art'].' X '.$line['qte'].'   = '.($line['art']->getPrix()*$line['qte']).'<br>' );
+            }
+            
+        }  
+                       
+    }
+?>
