44fe47ba5144e736af2f92bd2333c309807586b3
AbdallahChehabi <abd199308@gmail.com>
2023-11-10 09:17:35 +0100

    vehicule__AC

---
+++ POO/vehicule__AC/bateau.php
@@ -0,0 +1,30 @@
+<?php
+
+    include_once "vehicule.php";
+
+    class Bateau extends Vehicule
+    {
+        protected string $type = '';
+
+        public function __construct( string $nom, $type, $vitesseMax ) 
+        {
+            parent::__construct( $nom, $vitesseMax );
+            $this->type = $type;
+        }                   
+
+        public function __toString() 
+        {
+            return '(Bateau '.$this->nom.'  '.$this->famille.')'; 
+        }
+
+        public function demarrer()
+        {
+            print( "$this->nom : je leve l'ancre<br>" );
+        }
+
+        public function arreter()        
+        {
+            print( "$this->nom : je réduis l'allure et je jette l'ancre<br>" );
+        }
+     }
+?>
